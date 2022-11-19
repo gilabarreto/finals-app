2cc83ec73150f9bd6164b80b496f3424ddc2a8a9
Tom <chiatunglo@hotmail.com>
2022-11-18 22:57:47 -0500

    install react router and add routes for home and artist page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import ArtistPage from "./components/ArtistPage";
+import SearchPage from "./components/SearchPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <ArtistPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<SearchPage />}></Route>
+          <Route path="/artist" element={<ArtistPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/SearchPage.js
@@ -0,0 +1,7 @@
+export default function SearchPage() {
+  return (
+    <div>
+      <h1>This is Search Page</h1>
+    </div>
+  );
+}
