66e8eca30e44654a28d2aa76442898f87b4568ec
Brenda <brendamvreal@gmail.com>
2022-11-26 17:24:25 -0500

    Update file to the most recent code from branch.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,62 +6,84 @@ import searchIcon from "../icons/search.png";
 import axios from "axios";
 
 export default function SearchBar(props) {
-
   const [value, setValue] = useState("");
-  const handleChange = event => setValue(event.target.value)
+  const handleChange = event => setValue(event.target.value);
 
   const term = useDebounce(value, 700);
 
   useEffect(() => {
-
     if (term.length === 0) {
-      return
-    }
-    else {
-
+      return;
+    } else {
       //GET Request Setlist
-      axios.get('/rest/1.0/search/setlists', {
-        params: {
-          'artistName': `"${value}"`,
-          'p': '1'
-        },
-        headers: {
-          'Accept': 'application/json',
-          'x-api-key': 'eY_2IYBgy3ovn4sRZSqa9cTZy1nldhaUCvif'
-        }
-      })
-        .then((res) => {
-          props.setResults(res.data.setlist)
+      axios
+        .get("/rest/1.0/search/setlists", {
+          params: {
+            artistName: `"${value}"`,
+            p: "1",
+          },
+          headers: {
+            Accept: "application/json",
+            "x-api-key": "eY_2IYBgy3ovn4sRZSqa9cTZy1nldhaUCvif",
+          },
         })
-        .catch((err) => {
-          props.setResults([])
-          console.log("Setlist Get Resquest Error:", err)
+        .then(res => {
+          props.setResults(res.data.setlist);
         })
+        .catch(err => {
+          props.setResults([]);
+          console.log("Setlist Get Resquest Error:", err);
+        });
+
+      axios
+        .get(
+          "https://api.spotify.com/v1/artists/0TnOYISbd1XYRBk9myaseg/top-tracks",
+          {
+            params: {
+              market: "ES",
+            },
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+              Authorization: "1b96527ae88b428e9df149a9ef210091",
+            },
+          }
+        )
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log("Setlist Get Resquest Error:", err);
+        });
 
       // GET Request Ticketmaster
       // axios.get('https://app.ticketmaster.com/discovery/v2/events.json', {
-      axios.get('https://app.ticketmaster.com/discovery/v2/suggest', {
-        params: {
-          'keyword': `"${value}"`,
-          'segmentId': 'KZFzniwnSyZfZ7v7nJ', 
-          'sort': 'name,asc',
-          'apikey': 'kMv2pjo5bzSz5iyaz0h5aLqGnQcWyOSL'
-        }
-      })
-        .then((res) => {
-          props.setTicketmaster(res.data._embedded)
-          console.log(res.data._embedded)
+      axios
+        .get("https://app.ticketmaster.com/discovery/v2/suggest", {
+          params: {
+            keyword: `"${value}"`,
+            segmentId: "KZFzniwnSyZfZ7v7nJ",
+            sort: "name,asc",
+            apikey: "kMv2pjo5bzSz5iyaz0h5aLqGnQcWyOSL",
+          },
         })
-        .catch((err) => {
-          props.setTicketmaster([])
-          console.log("Ticketmaster Get Request Error", err)
+        .then(res => {
+          props.setTicketmaster(res.data._embedded);
+          console.log(res.data._embedded);
         })
+        .catch(err => {
+          props.setTicketmaster([]);
+          console.log("Ticketmaster Get Request Error", err);
+        });
     }
-  }, [term])
+  }, [term]);
 
   return (
     <div className="search">
-      <form className="input-container-search" onSubmit={event => event.preventDefault()}>
+      <form
+        className="input-container-search"
+        onSubmit={event => event.preventDefault()}
+      >
         <img className="searchIcon" src={searchIcon}></img>
         <input
           className="input-text-search"
